3557c2aff9ae287435485d09a45c76d13b0d743b
IljaRukin <74410869+IljaRukin@users.noreply.github.com>
2023-04-02 21:10:31 +0200

    Delete generate_hash.py

--- generate_hash.py
+++
@@ -1,6 +0,0 @@
-import hashlib, uuid
-salt = uuid.uuid4().hex
-password = input('type in your password: ')
-hashed_password = hashlib.sha512((password+salt).encode('utf-8')).hexdigest()
-print('hashed password: ',str(hashed_password))
-input('type any key to abort program')
